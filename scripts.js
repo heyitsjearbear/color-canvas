766e08fffac2504cf312bbb6250a6fc3234cac53
Jeremiah Desktop <jereberries1215@gmail.com>
2022-11-28 20:59:54 -0800

    Created function that creates one row of squares.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 //grab the container that we will eventually append to
-const gridContainer = document.querySelector('.grid');
+const gridContainer = document.querySelector(".grid");
 
-//for loop to make 4x4(16 divs)
-for (let i = 1; i <= 256; i++){
-    //create div referenced by newSquare
-    let newSquare = document.createElement('div');
-    newSquare.id = i;
-    newSquare.classList.add('square');
-    newSquare.textContent = i;
-    gridContainer.appendChild(newSquare);
-}
+//create a function that will create one row of passed parameter squares
+let createRow = (numOfSquares) => {
+    //create row div itself
+    let newRow = document.createElement("div");
+    newRow.classList.add("row");
+    //within the new row created, create 'numofSquares' divs
+    for (let i = 1; i <= numOfSquares; i++){
+        let newSquare = document.createElement("div");
+        newSquare.classList.add("row");
+        newSquare.textContent = i;
+        //append to row that was just created
+        newRow.appendChild(newSquare);
+    }
+    gridContainer.appendChild(newRow);
+};
+createRow(4);
