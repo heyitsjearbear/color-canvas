ea431c1cf1472c9c0bfc494d282b638d12cf774b
Jeremiah Desktop <jereberries1215@gmail.com>
2022-11-28 21:21:06 -0800

    Implemented creation of 1 row of 4 squares.

--- scripts.js
+++ scripts.js
@@ -2,18 +2,20 @@
 const gridContainer = document.querySelector(".grid");
 
 //create a function that will create one row of passed parameter squares
-let createRow = (numOfSquares) => {
+let createRow = (numOfSquares,rowNumber) => {
     //create row div itself
     let newRow = document.createElement("div");
     newRow.classList.add("row");
+    newRow.id = rowNumber;
     //within the new row created, create 'numofSquares' divs
     for (let i = 1; i <= numOfSquares; i++){
         let newSquare = document.createElement("div");
-        newSquare.classList.add("row");
+        newSquare.classList.add("square");
         newSquare.textContent = i;
+        
         //append to row that was just created
         newRow.appendChild(newSquare);
     }
     gridContainer.appendChild(newRow);
 };
-createRow(4);
+createRow(4,"row-1");
